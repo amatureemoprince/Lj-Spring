651508db4953d3b95a1856e1acc8920f849dad7a
lj <2069057986@qq.com>
2026-01-16 11:32:13 +0800

    build(deps): add CGLIB and Hutool for bean instantiation strategy

---
+++ src/main/java/com/lj/framwork/beans/factory/support/CglibSubclassingInstantiationStrategy.java
@@ -0,0 +1,9 @@
+package com.lj.framwork.beans.factory.support;/**
+  *@ClassName CglibSubclassingInstantiationStrategy
+  *@Description
+  *@Author Dark Chocolate 2069057986@qq.com
+  *@Date 2026/1/16 10:53
+  *@Version JDK 17
+*/
+public class CglibSubclassingInstantiationStrategy {
+}
---
+++ src/main/java/com/lj/framwork/beans/factory/support/InstantiationStrategy.java
@@ -0,0 +1,9 @@
+package com.lj.framwork.beans.factory.support;/**
+  *@ClassName InstantiationStrategy
+  *@Description
+  *@Author Dark Chocolate 2069057986@qq.com
+  *@Date 2026/1/16 10:52
+  *@Version JDK 17
+*/
+public interface InstantiationStrategy {
+}
